d621c2e18185a737584be43bb75e7cf13b1db1c4
paul-murithi <paulmurithi518@gmail.com>
2025-10-24 14:58:03 +0300

    feat: Add Course model

--- webapp/models/user.py
+++ webapp/models/user.py
@@ -1,3 +1,5 @@
+from enum import unique
+
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import backref
 
@@ -20,3 +22,16 @@ class User(db.Model):
         if role not in VALID_ROLES:
             raise ValueError(f"Invalid role {role}")
         self.role = role
+
+class Course(db.Model):
+    __tablename__ = 'course'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    code = db.Column(db.String(100), nullable=False, unique=True)
+    professor_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    program_id = db.Column(db.Integer, db.ForeignKey('programs.id'), nullable=True)  # TODO: make required later
+    semester = db.Column(db.Integer, nullable=False)
+
+    professor = db.relationship('User', back_populates='courses')
+    enrollments = db.relationship('Enrollment', back_populates='course', cascade='all, delete')
