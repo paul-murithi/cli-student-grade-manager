b2c70a839fac68c55914b0beab79a8a8493e30d5
paul-murithi <paulmurithi518@gmail.com>
2025-11-19 13:51:12 +0300

    view grades logic and UI for students

--- webapp/routes/grades.py
+++ webapp/routes/grades.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, redirect, url_for, request, flash, current_app
+from flask import Blueprint, redirect, url_for, request, flash, current_app, render_template
 from webapp.utils.decorators import role_required
 from webapp.utils.helpers import clean_form_data, get_letter_grade
 from webapp import db
 from webapp.models.models_file import Enrollment
-from flask_login import login_required
+from flask_login import login_required, current_user
 from datetime import datetime, timezone
 
 grades_bp = Blueprint('grades', __name__, url_prefix='/grades')
@@ -57,3 +57,15 @@ def grade_student():
         current_app.logger.error(f'An error occured: {e}')
 
     return redirect(url_for('dashboard.professor_dashboard'))
+
+@grades_bp.route('/view', methods=['GET'])
+@login_required
+@role_required('student')
+def view_grades():
+    enrollments = Enrollment.query.filter_by(user_id=current_user.id).all()
+    enrollments_list = list(enrollments) if enrollments else []
+    if not enrollments_list:
+        flash('No enrollments were found for this user', 'error')
+        current_app.logger.error(f'No enrollments were found for this user. UserId: {current_user.id}')
+        return render_template('enrollments/enrollment.html', enrollments=[])
+    return render_template('enrollments/enrollment.html', enrollments=enrollments_list)
